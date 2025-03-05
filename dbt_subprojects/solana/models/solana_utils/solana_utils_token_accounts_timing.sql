d3652b79fb81c5fdd12f85ff4664779a47fa1db1
tentabs00 <tentabs00@gmail.com>
2025-03-05 15:20:18 -0800

    feature: add base query

---
+++ dbt_subprojects/solana/models/solana_utils/solana_utils_token_accounts_timing.sql
@@ -0,0 +1,56 @@
+WITH token_offsetter AS (
+    -- adds helper column to identify when an address was assigned a new token
+    SELECT
+        *
+        , LAG(token_mint_address) OVER (PARTITION BY address, token_balance_owner ORDER BY block_time ASC) AS prev_token
+    FROM solana.account_activity
+)
+
+, pair_orderings AS (
+    -- identifies the ordering of each address-mint pairing with support for repeated pairings
+    SELECT
+        *
+        , SUM(
+            CASE
+                WHEN token_mint_address != prev_token THEN 1
+                ELSE 0
+            END
+          ) OVER (PARTITION BY address, token_balance_owner ORDER BY block_time ASC) AS token_pairing_rank
+    FROM token_offsetter
+)
+
+, account_activity_base AS (
+    -- flattens data to each address-mint pairing with start/end timestamps
+    SELECT
+        address
+        , token_balance_owner
+        , token_mint_address
+        , MIN(block_time) AS activity_start
+        , MAX(block_time) AS activity_end
+    FROM pair_orderings
+    GROUP BY 1, 2, 3, token_pairing_rank
+)
+
+, nft_addresses AS (
+    -- updated nft logic to exclude fungible token_standard types from nft classification
+    SELECT
+        account_mint
+    FROM tokens_solana.nft
+    WHERE
+        account_mint IS NOT NULL
+        -- without this filter the old table classified many fungible tokens (e.g. JUP,DRIFT) as account_type=nft
+        AND token_standard NOT IN ('Fungible', 'FungibleAsset')
+    GROUP BY 1
+)
+
+-- final table retains account_activity structure with additional start/end columns
+SELECT
+    aa.*
+    , CASE
+        WHEN nft.account_mint IS NOT NULL THEN 'nft'
+        ELSE 'fungible'
+      END AS account_type
+FROM account_activity_base aa
+LEFT JOIN nft_addresses nft
+    ON aa.token_mint_address = nft.account_mint
+ORDER BY address, activity_start;
